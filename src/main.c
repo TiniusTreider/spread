6e4b0bd4dae6e821edc1caa71dec17541bf8c342
Tinius Treider <timmytreider@outlook.com>
2026-05-14 22:44:48 +0200

    useless cli

--- src/main.c
+++ src/main.c
@@ -1,25 +1,9 @@
 #include "control.h"
-#include "error.h"
 
-#include <string.h>
-
-#define PARSE "parse"
-#define COMPARE "comp"
-
-#define ARGUMENT_COUNT "Needs 1 argument (\"" PARSE "\" or \"" COMPARE "\")"
-#define ARGUMENTS "Argument not \"" PARSE "\" or \"" COMPARE "\""
-
-int main(int argc, char **argv)
+int main(void)
 {
-        errorif(argc != 2, ARGUMENT_COUNT);
-        char *arg = argv[1];
-
-        if (strcmp(arg, PARSE) == 0)
-                parse();
-        else if (strcmp(arg, COMPARE) == 0)
-                compare();
-        else
-                error(ARGUMENTS);
+        parse();
+        compare();
 
         return 0;
 }
