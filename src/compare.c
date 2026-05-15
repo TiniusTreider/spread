67ad35b9000ffd422ecbd9c781c7b9839fe8fc52
Tinius Treider <timmytreider@outlook.com>
2026-05-15 23:02:00 +0200

    refactor

--- src/compare.c
+++ src/compare.c
@@ -2,16 +2,12 @@
 #include "csv.h"
 #include "stack.h"
 
+#include <stdbool.h>
 #include <stddef.h>
-
-static inline size_t range(struct csv a, struct csv b)
-{
-        /* TODO: find the range of days both sets have (bin search) */
-}
+#include <stdio.h>
 
 static inline void compare_stocks(struct csv a, struct csv b)
 {
-        size_t length = range(a, b);
 }
 
 void compare(void)
@@ -20,7 +16,7 @@ void compare(void)
         {
                 for (size_t b = a + 1; b < s_size(stocks); b++)
                 {
-                        compare_stocks(s_index(stocks, a), s_index(stocks, b));
+                        //compare_stocks(s_index(stocks, a), s_index(stocks, b));
                 }
         }
 
